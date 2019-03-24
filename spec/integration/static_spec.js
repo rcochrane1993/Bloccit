0ccdcd173c38bc20ed8efbb472f7f65ec652ca78
rcochrane1993 <35496351+rcochrane1993@users.noreply.github.com>
2019-03-24 13:35:00 -0400

    assignmentcompleted

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -12,10 +12,12 @@ describe("routes : static", () => {
         });
     });
 
-    describe("GET /about", (done) => {
+    describe("GET /about", () => {
         it("should return status code 200 and have 'About us' in the body of the response", () => {
-            expect(res.statusCode).toBe(200);
-            expect(body).toContain('About us');
+            request.get(base, (err,res,body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toContain('About us');
+            });
         });
     });
 });
--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,7 +1,7 @@
 module.exports = {
     index(req,res,next){
         res.render("static/index", {title: "Welcome to Bloccit"});
-    }
+    },
 
     aboutUs(req,res,next){
         res.render("static/aboutUs",{title: "About Us"});
